d0ec00c6c3b62e762a012bcb3d92171dd5bebb60
Ricardo E. Sanchez <ricardose0419@gmail.com>
2024-02-04 15:44:33 -0600

    fixed routing error

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const controller = require("./dishes.controller")
 
-router.route("/dishes")
+router.route("/")
     .get(controller.list)
     .post(controller.validateDish, controller.create);
 
-    router.route("/dishes/:dishId")
+    router.route("/:dishId")
     .get(controller.read)  // Handler for GET request
     .put(controller.validateDish, controller.dishExists, controller.update)  // Validation and update handlers
     .all(controller.methodNotAllowed); 
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const controller = require("./orders.controller");
 
-router.route("/orders")
+router.route("/")
   .get(controller.list)
   .post(controller.validateOrder, controller.create);
 
-router.route("/orders/:orderId")
+router.route("/:orderId")
   .get(controller.orderExists, controller.read)
   .put(controller.orderExists, controller.validateOrder, controller.validateUpdate, controller.update)
   .delete(controller.orderExists, controller.validateDelete, controller.delete);
